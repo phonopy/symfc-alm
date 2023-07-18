b881e89dbf15be27f469900a0a0d3f42d7bcaf47
naruse <naruse.takuya.ac@gmail.com>
2023-07-18 12:50:58 +0900

    Add import LinearModel, RidgeRegression, standardize_data to src/__init__.py file

--- src/symfc_alm/__init__.py
+++ src/symfc_alm/__init__.py
@@ -1,5 +1,8 @@
 """Import SymfcAlm."""
 from .symfc_alm import CellDataset  # noqa: F401
 from .symfc_alm import DispForceDataset  # noqa: F401
+from .symfc_alm import LinearModel  # noqa: F401
+from .symfc_alm import RidgeRegression  # noqa: F401
 from .symfc_alm import SymfcAlm  # noqa: F401
 from .symfc_alm import read_dataset  # noqa: F401
+from .symfc_alm import standardize_data  # noqa: F401
