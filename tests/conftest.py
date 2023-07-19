009629dbb42c162201a559e2101109776e2cd5af
naruse <naruse.takuya.ac@gmail.com>
2023-07-19 14:01:07 +0900

    Add --runbig option

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,29 @@ from symfc_alm import CellDataset, DispForceDataset, read_dataset
 cwd = Path(__file__).parent
 
 
+def pytest_addoption(parser):
+    """Add command option to pytest."""
+    parser.addoption(
+        "--runbig", action="store_true", default=False, help="run big tests"
+    )
+
+
+def pytest_configure(config):
+    """Set up marker big."""
+    config.addinivalue_line("markers", "big: mark test as big to run")
+
+
+def pytest_collection_modifyitems(config, items):
+    """Add mechanism to run with --runbig."""
+    if config.getoption("--runbig"):
+        # --runbig given in cli: do not skip slow tests
+        return
+    skip_big = pytest.mark.skip(reason="need --runbig option to run")
+    for item in items:
+        if "big" in item.keywords:
+            item.add_marker(skip_big)
+
+
 @pytest.fixture(scope="session")
 def nacl_222_structure() -> CellDataset:
     """Return NaCl 2x2x2 supercell structure."""
